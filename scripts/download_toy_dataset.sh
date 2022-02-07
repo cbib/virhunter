9fcf6fa4e4cfa6c22262ec1e75d7868678194e27
Greg <gregoruar@github.com>
2022-02-07 16:34:36 +0100

    fix test

--- scripts/download_toy_dataset.sh
+++ scripts/download_toy_dataset.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 wget https://zenodo.org/record/5957275/files/toy_dataset.tar.gz
-tar -xvf toy_dataset.tar.gz && mv test toy_dataset
+mkdir toy_dataset
+tar -xf toy_dataset.tar.gz -C toy_dataset --strip-components=1
 mkdir toy_dataset_out
 rm toy_dataset.tar.gz
