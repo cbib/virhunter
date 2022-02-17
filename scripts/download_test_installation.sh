c8d0328569047217d105fe74e8718d3bb25a627f
Greg <gregoruar@github.com>
2022-02-17 11:35:14 +0100

    bug fix

--- scripts/download_test_installation.sh
+++ scripts/download_test_installation.sh
@@ -2,6 +2,6 @@
 
 wget https://zenodo.org/record/5957275/files/toy_dataset.tar.gz
 mkdir -p test_installation
-tar -xf toy_dataset.tar.gz -C toy_dataset --strip-components=1
+tar -xf toy_dataset.tar.gz -C test_installation --strip-components=1
 mkdir -p test_installation_out
 rm toy_dataset.tar.gz
